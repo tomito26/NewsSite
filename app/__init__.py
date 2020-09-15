83383e0cb384d7e020369bdfd5ce6b7b1c8f4c52
tomito26 <tommybwah@gmail.com>
2020-09-15 16:25:55 +0300

    added error.py file and fourowfor.html file

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,21 @@
 from flask import Flask
-from .config import DevConfig
+from config import config_options
 from flask_bootstrap  import Bootstrap
 
-# Initiallizing the application
-app = Flask(__name__,instance_relative_config= True)
+bootstrap = Bootstrap()
+def create_app(config_name):
+    
+    app = Flask(__name__)
 
-# Setting up configuration
-app.config.from_object(DevConfig)
-app.config.from_pyfile('config.py')
+    # Creating app configurations
+    app.config.from_object(config_options[config_name])
 
-# Initializing flask extensions
-bootstrap = Bootstrap(app)
 
-from app import views
+    # Initializing flask extensions
+    bootstrap.init_app(app)
+
+    # Registering the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+    
+    return app
